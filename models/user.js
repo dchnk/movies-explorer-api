4bd8d6b21101b667bacb2a89f0cac82ed11ea8e6
dchnk <seroxe2@yandex.ru>
2023-08-09 15:21:12 +0700

    fix: исправить валидацию на уровне схемы

--- models/user.js
+++ models/user.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+const { emailRegexTest } = require('../utils/linkRegexTest');
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    validate: {
+      validator(v) {
+        return emailRegexTest(v);
+      },
+    },
     unique: true,
   },
   password: {
--- utils/linkRegexTest.js
+++ utils/linkRegexTest.js
@@ -1,7 +1,12 @@
 const RegExp = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\\/])*)?/;
+const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const linkRegexTest = (link) => {
   RegExp.test(link);
 };
 
-module.exports = { linkRegexTest, RegExp };
+const emailRegexTest = (email) => {
+  emailRegExp.test(email);
+}
+
+module.exports = { linkRegexTest, emailRegexTest, RegExp };
